655b353abca2371e6aa7012106cf744c657434fe
Emsal CENGİZ <30407530+emsalcengiz@users.noreply.github.com>
2021-08-25 16:41:26 +0300

    updated

--- src/db_connection.py
+++ src/db_connection.py
@@ -3,8 +3,8 @@ import psycopg2
 def get_connection():
     return psycopg2.connect(
         user = "postgres",
-        password = "1234",
-        host = "postgres",
+        password = "****",
+        host = "xyz",
         port = "5432",
         database = "postgres"
     )
